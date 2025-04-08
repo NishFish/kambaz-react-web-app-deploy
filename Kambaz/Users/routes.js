fea062c705da4a41faecbc36f798371cacb4695f
Nishanth Gopinath <gopinath.ni@northeastern.edu>
2025-04-08 11:18:20 -0400

    fixed courses issue

--- Kambaz/Users/routes.js
+++ Kambaz/Users/routes.js
@@ -103,7 +103,6 @@ export default function UserRoutes(app) {
         const courses = courseDao.findCoursesForEnrolledUser(userId);
         res.json(courses);
     };
-    app.get("/api/users/:userId/courses", findCoursesForEnrolledUser);
 
     const createCourse = async (req, res) => {
         const currentUser = await req.session["currentUser"];
@@ -160,4 +159,5 @@ export default function UserRoutes(app) {
     app.post("/api/users/signin", signin);
     app.post("/api/users/signout", signout);
     app.post("/api/users/profile", profile);
+    app.get("/api/users/:userId/courses", findCoursesForEnrolledUser);
 }
